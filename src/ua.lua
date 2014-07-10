75611a2dd0bcfa5090f1892acc820e6b1539a7d7
Frédéric ROBINET <robinef@gmail.com>
2014-07-11 00:10:16 +0200

    fixing class bug

--- src/ua.lua
+++ src/ua.lua
@@ -1,41 +1,72 @@
-function readAll(file)
+json = require('json')
+lib = require('rex_pcre')
+
+ua = {}
+ua.__index = ua
+
+
+--[[
+    Class ctor
+    @param string JSON user agent config file
+    @return class
+]]--
+function ua.create(file)
+    local ualoc = {}        -- our new object
+    setmetatable(ualoc,ua)  -- make UA handle lookup
+    
+    ualoc.content = ua.readAll(file)
+    if ualoc.content then
+        ualoc.result = json.decode(ualoc.content)
+    end
+    return ualoc
+end
+
+--[[
+    Read file and bufferize content
+    @param string Filename
+    @return string File content
+]]--
+function ua.readAll(file)
     local f = io.open(file, "rb")
-    local content = f:read("*all")
-    f:close()
+    local content = ""
+    if f then
+         content = f:read("*all")
+        f:close()
+    end
     return content
 end
 
 --[[
-
+    Test if user agent is Iphone
+    @param string userAgent
+    @return bool
 ]]--
-json = require('json')
-lib = require('rex_pcre')
-content = readAll('Mobile_Detect.json')
-result = json.decode(content)
-
-
-function isPhone(userAgent)
-	for key,value in pairs(result['uaMatch']['phones']) do
-		res = lib.match(userAgent, value)
-    	if res then
-    		return true
-    	end
-	end
-	return false
+function ua:isPhone(userAgent)
+    if self.result then
+        for key,value in pairs(self.result['uaMatch']['phones']) do
+            res = lib.match(userAgent, value)
+            if res then
+                return true
+            end
+        end
+    end
+    return false
 end
 
-function isTablet(userAgent)
-	for key,value in pairs(result['uaMatch']['tablets']) do
-		res = lib.match(userAgent, value)
-    	if res then
-    		return true
-    	end
-	end
-	return false
+--[[
+    Test if user agent is Tablet
+    @param string userAgent
+    @return bool
+]]--
+function ua:isTablet(userAgent)
+    
+    if self.result then
+        for key,value in pairs(self.result['uaMatch']['tablets']) do
+            res = lib.match(userAgent, value)
+            if res then
+                return true
+            end
+        end
+    end
+    return false
 end
-
-local userAgentIphone = "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3"
-local userAgentIpad = "Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3"
-
-print(isPhone(userAgentIphone))
-print(isPhone(userAgentIpad))
--- test/is_tablet.lua
+++ test/is_tablet.lua
@@ -1,7 +1,7 @@
 package.path = package.path .. ";../src/?.lua"
 require 'ua'
 
-detect = ua.create('/Users/fred/Documents/ua-lua/ua-lua/src/Mobile_Detect.json')
+detect = ua.create('../src/Mobile_Detect.json')
 
-local userAgentIpad = "Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3"
-print(ua.isPhone(userAgentIpad))
+userAgentIpad = "Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3"
+print(detect:isTablet(userAgentIpad))
